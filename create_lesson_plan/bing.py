350538043f491b20c4bac655b72d0ea818b7f174
rishabhranawat <rishabhranawat12345@gmail.com>
2017-02-24 17:27:06 -0500

    not implementing summsrch.py and improving query methodologies

--- create_lesson_plan/bing.py
+++ create_lesson_plan/bing.py
@@ -13,10 +13,11 @@ def bing_search(query, search_type, query_original):
     credentials = (':%s' % key).encode('base64')[:-1]
     auth = 'Basic %s' % credentials
     #url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=10&$format=json'
+    print(query)
     if("wikipedia" in query):
-        url = 'https://api.datamarket.azure.com/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=10&$format=json'
+        url = 'https://api.datamarket.azure.com/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=3&$format=json'
     else:
-        url = 'https://api.datamarket.azure.com/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=20&$format=json'
+        url = 'https://api.datamarket.azure.com/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=5&$format=json'
     try:
       #proxy settings for IITD network
      #proxy = urllib2.ProxyHandler({'https': 'https://tt1140924:decode#thecode@proxy22.iitd.ernet.in:3128'})
