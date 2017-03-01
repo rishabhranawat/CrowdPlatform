ed6471b2cfe6cf160a8bd076f46fff1d28e17236
rishabhranawat <rishabhranawat12345@gmail.com>
2017-03-01 12:42:34 -0500

    fixing searc lesson plan

--- create_lesson_plan/forms.py
+++ create_lesson_plan/forms.py
@@ -3,14 +3,10 @@ from django import forms
 class UploadLessonPlanForm(forms.Form):
 	GRADE_CHOICES = (
         ('Undergraduate', 'Undergraduate'),
+        ('Graduate', 'Graduate'),
     )
 	subject_name = forms.CharField(label='Subject Name')
 	course_name = forms.CharField(label='Course Name')
 	input_title = forms.CharField(label='Lesson Title')
 	input_grade = forms.ChoiceField(label='Grade', choices=GRADE_CHOICES)
 	input_bullets = forms.CharField(label='Bullets', widget=forms.Textarea)
-	# upload = forms.FileField(label='Select File(s)', 
-	# 	widget=forms.ClearableFileInput(attrs={
-	# 		'multiple': True,
-	# 		'id': "upload",
-	# 		'name': 'upload[]'}))
