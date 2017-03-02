2805bc84a7f09f86cc6858b3476182e0559fb28e
rishabhranawat <rishabhranawat12345@gmail.com>
2017-03-02 11:45:03 -0500

    testing

--- yesua1/settings.py
+++ yesua1/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "create_lesson_plan",
     "create_lesson_plan.apps",
+    
     #"create_lesson_plan.apps.CreateLessonPlanConfig",
 
     # theme
