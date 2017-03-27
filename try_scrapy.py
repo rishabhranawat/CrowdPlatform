027e4eb833e3d68bc2405dc7f3438a996622c134
rishabhranawat <rishabhranawat12345@gmail.com>
2017-03-27 13:24:47 -0400

    level 1 BFS crawler using scrapy working

--- try_scrapy.py
+++ try_scrapy.py
@@ -1,6 +1,9 @@
 import scrapy
 from scrapy.item import Item, Field
-from scrapy.crawler import CrawlerProcess
+
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
 
 
 class LinkObject(Item):
@@ -14,18 +17,36 @@ class WikipediaSpider(scrapy.Spider):
 	def parse(self, response):
 		for url in response.css('a'):
 			link_obj = LinkObject()
+			link_obj['link'] = response.urljoin(url.xpath('@href').extract_first())
+			link_obj['content'] = response.text
+			link_objs.append(link_obj)
+
+class Level1Spider(scrapy.Spider):
+	name = "level1spider"
+	start_urls = []
+
+	def __init__(self):
+		for link_obj in link_objs:
+			url = link_obj['link']
+			if(url is not None):
+				self.start_urls.append(link_obj['link'])
+
+	def parse(self, response):
+		for url in response.css('a'):
+			link_obj = LinkObject()
 			link_obj['link'] = url.xpath('@href').extract_first()
 			link_obj['content'] = response.text
 			link_objs.append(link_obj)
 
 
-class Level1Spider(scrapy)
-	name = "level1spider"
-	start_urls =[]
+configure_logging()
+runner = CrawlerRunner()
 
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(WikipediaSpider)
+    yield runner.crawl(Level1Spider)
+    reactor.stop()
 
-process.crawl(WikipediaSpider)
-process.start()
+crawl()
+reactor.run() 
