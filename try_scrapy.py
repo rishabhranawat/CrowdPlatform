8209f8b8bb4287fab70abdc27fa5ee3053891301
rishabhranawat <rishabhranawat12345@gmail.com>
2017-03-31 13:06:13 -0400

    indexed a bunch of pages. need to set a unique key on url. clean up the command and move on to query generatro

--- try_scrapy.py
+++ try_scrapy.py
@@ -5,15 +5,11 @@ from twisted.internet import reactor, defer
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 
-from create_lesson_plan.models import OfflineDocument
-from django.utils.html import strip_tags()
-
+from django.utils.html import strip_tags
 
 class LinkObject(Item):
     link = Field()
-    content = Field()
 
-all_links = []
 
 # Has all links from 1st page
 link_objs = []
@@ -22,14 +18,15 @@ class WikipediaSpider(scrapy.Spider):
 	start_urls = ['https://en.wikipedia.org/wiki/Portal:Computer_science']
 	count = 0
 	def parse(self, response):
+		
 		for url in response.css('a'):
 			self.count +=1 
 			if(self.count == 10):
 				return link_objs
 			link_obj = LinkObject()
 			link_obj['link'] = response.urljoin(url.xpath('@href').extract_first())
-			link_obj['content'] = strip_tags(response.text)
 			link_objs.append(link_obj)
+
 		all_links.extend(link_objs)
 		return link_objs
 
@@ -45,32 +42,27 @@ class Level1Spider(scrapy.Spider):
 		link_objs = []
 
 	def parse(self, response):
+
 		for url in response.css('a'):
 			link_obj = LinkObject()
 			link_obj['link'] = url.xpath('@href').extract_first()
-			link_obj['content'] = strip_tags(response.text)
+			# link_obj['content'] = strip_tags(response.text)
 			link_objs.append(link_obj)
 		all_links.extend(link_objs)
 
-class Level2Spider(scrapy.Spider):
-	name = "level2spider"
-	start_urls = []
-
-	def __init__(self):
 
 
 
 
-# configure_logging()
-# runner = CrawlerRunner()
+configure_logging()
+runner = CrawlerRunner()
 
-# @defer.inlineCallbacks
-# def crawl():
-#     yield runner.crawl(WikipediaSpider)
-#     yield runner.crawl(Level1Spider)
-#     reactor.stop()
-
-# crawl()
-# reactor.run() 
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(WikipediaSpider)
+    # yield runner.crawl(Level1Spider)
+    reactor.stop()
+crawl()
+reactor.run() 
 
 print(len(link_objs))
