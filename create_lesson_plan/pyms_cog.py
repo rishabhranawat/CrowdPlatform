bb52127078462740f4e0aecea5770aa0386ea8de
rishabhranawat <rishabhranawat12345@gmail.com>
2017-04-08 11:46:08 -0400

    wrote cleaner bing api version..i function

---
+++ create_lesson_plan/pyms_cog.py
@@ -0,0 +1,24 @@
+from py_ms_cognitive import PyMsCognitiveWebSearch
+
+def bing_search(query, limit):
+	all_links = []
+	try:
+		API_KEY ='8d06ebb959e54714bb2b5a8089704244'
+		search_service = PyMsCognitiveWebSearch(API_KEY, 
+			query, 
+			custom_params='&mkt=es-MX')
+		response = search_service.search(
+			limit=limit, 
+			format='json')
+		for link in response:
+			details = link.json
+			link_details = {
+				'title': details['name'],
+				'Url': details['url'],
+				'Description': details['snippet']
+			}
+			all_links.append(link_details)
+		return all_links
+	except:
+		print "Error when querying bing"
+		return all_links
