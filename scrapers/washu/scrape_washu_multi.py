64812a5330b8421e919155cb9a159203dbd894fe
rishabhranawat <rishabhranawat12345@gmail.com>
2017-04-19 11:58:27 -0400

    modularizing and finishing download part

--- scrapers/washu/scrape_washu_multi.py
+++ scrapers/washu/scrape_washu_multi.py
@@ -5,56 +5,9 @@ import re
 import urlparse
 import json
 import time
-import scraper_utils
+from scraper_utils import get_fro_links
+from functools import partial
 
-# Utils
-def is_abs(url):
-	return bool(urlparse.urlparse(url).netloc)
-
-def check_url(url, host_url):
-	if(host_url == url or url in all_course_pages):
-		return ["circle", False]
-	if(host_url in url and host_url != url):
-		return ["abs", True]
-	elif("mailto:" in url):
-		return ["mail", False]
-	else:
-		return ["rel", not (is_abs(url))]
-
-def get_absolute_path(host, rel):
-	return urlparse.urljoin(host, rel)
-
-def check_if_pdf(response):
-	return response.headers['content-type']=='application/pdf'
-		
-
-# Getting the fro_links
-def get_fro_links(course_home_page_url):
-	# Soup Boiler Plate
-	course_home_page_response = requests.get(course_home_page_url)	
-	course_home_page = course_home_page_response.content
-	course_home_page_soup = BeautifulSoup(course_home_page, 'html.parser')
-	course_home_page_links = course_home_page_soup.findAll('a')
-	
-	# Getting the links from course_home_page
-	course_home_page_fro_links = set()
-	for link_fro_course_home in course_home_page_links:
-		try:
-			link_fro_course_home = link_fro_course_home.get("href")
-			link_fro_course_home = link_fro_course_home.lstrip()
-
-			typ, cond = check_url(link_fro_course_home, course_home_page_url)
-			if(cond):
-				link_to_add = link_fro_course_home
-				if(typ == "rel"):
-					link_to_add = get_absolute_path(course_home_page_url, 
-						link_fro_course_home)
-				if(course_home_page_url != link_to_add):
-					course_home_page_fro_links.add(link_to_add)
-		except:
-			pass
-
-	return course_home_page_fro_links
 
 # def get_files_links(final_page_url):
 # 	page_response = requests.get(final_page_url)
@@ -71,17 +24,12 @@ def get_all_course_pages():
 			all_course_pages.extend(pages)
 	return all_course_pages
 
-all_course_pages = get_all_course_pages()
 def get_all_content_urls():
-	
+	all_course_pages = get_all_course_pages()
 	p = Pool(8)
-	all_for_links = list(p.map(get_fro_links, all_course_pages[:30]))
+	func = partial(get_fro_links, all_course_pages)
+	all_for_links = list(p.map(func, all_course_pages[:1]))
 	ll = set()
 	for each in all_for_links:
 		ll = ll | each
-	return ll
-
-content_urls = get_all_content_urls()
-p = Pool(8)
-p.map(scraper_utils.download_files_load_es, content_urls)
-
+	return ll, all_course_pages
--- scrapers/washu/scraper_utils.py
+++ scrapers/washu/scraper_utils.py
@@ -13,19 +13,48 @@ def get_file_type(url, response):
 def get_sha_encoding(content):
 	return 1
 
-def save_doc_in_els():
+def is_abs(url):
+	return bool(urlparse.urlparse(url).netloc)
 
-def download_files_load_es(content_page_url):
-	content_page_response = requests.get(content_page_url)
-	content_page = content_page_response.content
-	content_page_soup = BeautifulSoup(content_page, 'html.parser')
+def check_url(url, host_url, all_course_pages):
+	if(host_url == url or url in all_course_pages):
+		return ["circle", False]
+	if(host_url in url and host_url != url):
+		return ["abs", True]
+	elif("mailto:" in url):
+		return ["mail", False]
+	else:
+		return ["rel", not (is_abs(url))]
 
-	file_type = get_file_type(content_page_url, content_page_response)
+def get_absolute_path(host, rel):
+	return urlparse.urljoin(host, rel)
 
-	if(file_type == "application/pdf"):
+def check_if_pdf(response):
+	return response.headers['content-type']=='application/pdf'
+		
+def get_fro_links(all_course_pages, course_home_page_url):
+	# Soup Boiler Plate
+	course_home_page_response = requests.get(course_home_page_url)	
+	course_home_page = course_home_page_response.content
+	course_home_page_soup = BeautifulSoup(course_home_page, 'html.parser')
+	course_home_page_links = course_home_page_soup.findAll('a')
+	
+	# Getting the links from course_home_page
+	course_home_page_fro_links = set()
+	for link_fro_course_home in course_home_page_links:
+		try:
+			link_fro_course_home = link_fro_course_home.get("href")
+			link_fro_course_home = link_fro_course_home.lstrip()
 
+			typ, cond = check_url(link_fro_course_home, course_home_page_url, all_course_pages)
+			if(cond):
+				link_to_add = link_fro_course_home
+				if(typ == "rel"):
+					link_to_add = get_absolute_path(course_home_page_url, 
+						link_fro_course_home)
+				if(course_home_page_url != link_to_add):
+					course_home_page_fro_links.add(link_to_add)
+		except:
+			pass
 
-
-
-
-
+	return course_home_page_fro_links
