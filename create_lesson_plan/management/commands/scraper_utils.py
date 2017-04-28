b36f03866ec46eece28a738f98941d4aba98f5d4
rishabhranawat <rishabhranawat12345@gmail.com>
2017-04-28 08:53:41 -0400

    changes

--- create_lesson_plan/management/commands/scraper_utils.py
+++ create_lesson_plan/management/commands/scraper_utils.py
@@ -8,39 +8,13 @@ import json
 import time
 from time import sleep
 import hashlib
+from create_lesson_plan.models import OfflineDocument
+from django.core.files import File
 
 
 FILE_TYPES = ["application/pdf"]
 WASHU_STOP_URLS = ["http://courses.cs.washington.edu/", "/", 
 "http://cs.stanford.edu/academics/courses"]
-
-def download_files_load_es(all_course_pages, level, university,subject,content_page_url):
-	content_page_response = get_page_content_response(content_page_url)
-	if(content_page_response != None):
-		content_page = content_page_response.content
-		content_page_soup = BeautifulSoup(content_page, 'html.parser')
-
-		file_type = get_file_type(content_page_url, content_page_response)
-
-		# TO:DO -- Download
-		if(file_type in FILE_TYPES):
-			file_name = content_page_url.split("/")[-1]
-			f, response = download_pdf_file(content_page_url, file_name)
-			create_offline_document_object(content_page_url, 
-				response.read(), 
-				university, subject, 
-				f, file_name)
-			print(content_page_url)
-			return set()
-		elif(file_type not in FILE_TYPES and level == 1):
-			return get_fro_links(all_course_pages, content_page_url)
-		elif(file_type not in FILE_TYPES and level == 2):
-			create_offline_document_object(content_page_url, content_page_soup.content, 
-				university, subject)
-			print(content_page_url)
-	else:
-		if(level == 1): return set()
-		if(level == 2): pass
 
 
 def create_offline_document_object(content_page_url, content, univeristy, subject, 
@@ -52,18 +26,48 @@ def create_offline_document_object(content_page_url, content, univeristy, subjec
 		content=content)
 	if(f): 
 		off_doc.attachment.save(file_name, File(open(file_name, 'r')))
-		os.remove(file_name)		
 	off_doc.save()
 	print(str(off_doc.pk)+" "+off_doc.link)
 	return True
 
+def download_files_load_es(all_course_pages, level, university,subject,content_page_url):
+	content_page_response = get_page_content_response(content_page_url)
+	if(content_page_response != None):
+		try:
+			content_page = content_page_response.content
+			file_type = get_file_type(content_page_url, content_page_response)
 
-def download_pdf_file(download_url, name):
-    response = urllib2.urlopen(download_url)
-    file = open(name, 'w')
-    file.write(response.read())
-    file.close()
-    return file, response
+			if(file_type in FILE_TYPES):
+				file_name = content_page_url.split("/")[-1]
+				
+				f = download_pdf_file(content_page_url, file_name, 
+					content_page_response)
+				print(f)
+				create_offline_document_object(content_page_url, 
+					content_page_response.content, 
+					university, subject, 
+					f, file_name)
+				return set()
+			elif(file_type not in FILE_TYPES and level == 1):
+				return get_fro_links(all_course_pages, content_page_url, content_page_response)
+			elif(file_type not in FILE_TYPES and level == 2 and "text/html" in file_type):
+				content_page_soup = BeautifulSoup(content_page, 'html.parser')
+				create_offline_document_object(content_page_url, content_page_soup.content, 
+					university, subject)
+		except:
+			print(content_page_url)
+			return set()
+
+	else:
+		if(level == 1): return set()
+		if(level == 2): pass
+
+def download_pdf_file(download_url, name, response):
+	response = urllib2.urlopen(download_url)
+	file = open(name, 'w')
+	file.write(response.read())
+	file.close()
+	return file
 
 def get_file_type(url, response):
 	return response.headers['content-type']
@@ -72,17 +76,16 @@ def get_sha_encoding(content):
 	return 1
 
 def get_page_content_response(url):
-	print(url)
 	counter = 0
-	page_response = None
-	while(counter <=5 and page_response == None):
-		print(page_response)
-		counter += 1
-		try:
-			page_response = requests.get(url)
-		except:
-			sleep(1)
-			continue
+	page_response = requests.get(url)
+	# while(counter <=2 and page_response == None):
+	# 	print(page_response)
+	# 	counter += 1
+	# 	try:
+	# 		page_response = requests.get(url)
+	# 	except:
+	# 		sleep(1)
+	# 		continue
 	return page_response
 
 def is_abs(url):
@@ -106,12 +109,10 @@ def get_absolute_path(host, rel):
 def check_if_pdf(response):
 	return response.headers['content-type']=='application/pdf'
 		
-def get_fro_links(all_course_pages, course_home_page_url):
+def get_fro_links(all_course_pages, course_home_page_url, course_home_page_response):
 	# Soup Boiler Plate
-	
-	course_home_page_response = get_page_content_response(course_home_page_url)	
+		
 	if(course_home_page_response != None):
-
 		course_home_page = course_home_page_response.content
 		course_home_page_soup = BeautifulSoup(course_home_page, 'html.parser')
 		course_home_page_links = course_home_page_soup.findAll('a')
--- create_lesson_plan/management/commands/stanford_scrape.py
+++ create_lesson_plan/management/commands/stanford_scrape.py
@@ -31,10 +31,35 @@ def get_all_sub_level(content_page_urls, all_course_pages, typ):
 	else:
 		p.map(func, content_page_urls)
 
+
+def get_sub_level(all_course_pages, level, university, subject, content_page_url):
+	new_links_or_none = download_files_load_es(all_course_pages, level, university, subject, content_page_url)
+	return new_links_or_none
+
 class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
-		content_page_url = "http://www.scs.stanford.edu/17wi-cs140/"
-		course_page = "http://www.scs.stanford.edu/17wi-cs140/"
+		content_page_url = "http://web.stanford.edu/class/cs103/"
+		course_page = "http://web.stanford.edu/class/cs103/"
 
-		content_page_urls = get_all_sub_level([content_page_url], [course_page], 1)
-		content_page_urls_2 = get_all_sub_level([content_page_urls], [course_page], 2)
+		course_home_page_response = requests.get(content_page_url)
+		home_page_urls = list(get_fro_links([course_page], 
+			content_page_url, course_home_page_response))[:1]
+
+		all_level_1_links = set()
+		for each_url in home_page_urls:
+			new_links = get_sub_level([course_page], 1, 
+				"New York University", "Computer Science", each_url)
+			time.sleep(2)
+
+			if(new_links != None):
+				all_level_1_links = all_level_1_links | new_links
+		
+		all_level_1_links = list(all_level_1_links)
+		
+
+		for final_depth_url in all_level_1_links:
+			print(final_depth_url)
+
+			get_sub_level([course_page], 2, 
+				"New York University", "Computer Science", final_depth_url)
+			time.sleep(2)
