80455f0778ffa48740640d0f1a2c2dee8fafa08b
rishabhranawat <rishabhranawat12345@gmail.com>
2017-05-23 10:02:33 +0530

    fixed

--- create_lesson_plan/pyms_cog.py
+++ create_lesson_plan/pyms_cog.py
@@ -6,16 +6,18 @@ def bing_search(query, limit):
 		API_KEY ='8d06ebb959e54714bb2b5a8089704244'
 		search_service = PyMsCognitiveWebSearch(API_KEY, 
 			query, 
-			custom_params='&mkt=es-MX')
+			custom_params='&mkt=en-US')
 		response = search_service.search(
 			limit=limit, 
 			format='json')
+
 		for link in response:
 			details = link.json
 			link_details = {
 				'title': details['name'],
 				'Url': details['url'],
-				'Description': details['snippet']
+				'Description': details['snippet'],
+				'display_url': details['displayUrl']
 			}
 			all_links.append(link_details)
 		return all_links
