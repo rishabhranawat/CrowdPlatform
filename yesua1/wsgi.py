c9aa8b27028d3f9506130cc3c1ef3b3d28a58347
rishabhranawat <rishabhranawat12345@gmail.com>
2017-08-10 16:48:55 -0400

    config

--- yesua1/wsgi.py
+++ yesua1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CrowdPlatform project.
+WSGI config for mdem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-import django
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yesua1.settings")
-#django.setup()
-import yesua1.startup as startup
-startup.run()
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
