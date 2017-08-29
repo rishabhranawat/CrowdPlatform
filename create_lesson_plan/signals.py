01b4d5b110616d39d38a7d0dbcd7a61eeef7d6ae
rishabhanawat <rishabhranawat12345@gmail.com>
2017-08-29 13:46:27 -0400

    Removal

--- create_lesson_plan/signals.py
+++
@@ -1,8 +0,0 @@
-from create_lesson_plan.models import OfflineDocument
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=OfflineDocument)
-def index_offline_document(sender, instance, **kwargs):
-	instance.indexing()
-
