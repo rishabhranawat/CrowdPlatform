07039aa7af556c67bdef3fb77e74d8514689c49f
rishabhranawat <rishabhranawat12345@gmail.com>
2017-10-28 10:28:07 -0400

    chagnes

--- create_lesson_plan/graph_query/query_formulator_poc.py
+++ create_lesson_plan/graph_query/query_formulator_poc.py
@@ -12,39 +12,38 @@ import nltk
 
 class GraphQueryFormulator:
 	def __init__(self):
-		pass
+		self.kg = nx.read_gpickle("create_lesson_plan/graph_query/algorithms.gpickle")
 	
 	def get_queries(self, query):
-		kg = nx.read_gpickle("create_lesson_plan/graph_query/algorithms.gpickle")
-		return self.query_formulator(kg, query)	
+		return self.query_formulator(query)	
 		
-	def get_closest_distance_node(self, query, kg):
-	    nodes = kg.nodes()
+	def get_closest_distance_node(self, query):
+	    nodes = self.kg.nodes()
 	    mi, val = None, None
 	    for node in nodes:
 		dist = nltk.distance.edit_distance(node, query)
 		if(mi == None or dist < mi): 
 		    mi = dist
 		    val = node
-		if(query in node and (kg.node[node]["NodeType"] == "ConceptNode" or query not in val)): 
+		if(query in node and (self.kg.node[node]["NodeType"] == "ConceptNode" or query not in val)): 
 		    return node
 	    return val
 
-	def get_closest_node(self, query, kg):
-	    if(query in kg.nodes()): 
-		return query, kg.node[query]
+	def get_closest_node(self, query):
+	    if(query in self.kg.nodes()): 
+		return query, self.kg.node[query]
 	    else: 
-		node_label = self.get_closest_distance_node(query, kg)
-		return node_label, kg.node[node_label]
+		node_label = self.get_closest_distance_node(query)
+		return node_label, self.kg.node[node_label]
 
-	def query_formulator(self, kg, query):
+	def query_formulator(self, query):
 	    print(query)
 	    queries = []
-	    current_node, node = self.get_closest_node(query, kg)
-	    children_neighbours = kg.neighbors(current_node)
+	    current_node, node = self.get_closest_node(query)
+	    children_neighbours = self.kg.neighbors(current_node)
 	    
 	    print(current_node, node, children_neighbours)
 	    for child in children_neighbours:
-		queries.append(current_node+" "+child)
+			queries.append(current_node+" "+child)
 	    return queries
 
