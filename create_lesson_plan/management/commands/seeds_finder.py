1f196ec675bdb1132780a9e1f8df2f914cb4a8fc
rishabhranawat <rishabhranawat12345@gmail.com>
2017-10-28 12:45:04 -0400

    seeds finder working and creating some amazing seeds

--- create_lesson_plan/management/commands/seeds_finder.py
+++ create_lesson_plan/management/commands/seeds_finder.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from multiprocessing import Pool
 from functools import partial
 from Queue import Queue
+import requests
 
 import threading
 
@@ -10,35 +11,60 @@ from create_lesson_plan.models import OfflineDocument
 from create_lesson_plan.graph_query.query_formulator_poc import GraphQueryFormulator
 from create_lesson_plan.pyms_cog import *
 
+
+
 class Command(BaseCommand):
 	def __init__(self):
 	    self.links = set()
-            
-        def get_graph_nodes(self):
-            gqf = GraphQueryFormulator()
-            nodes = gqf.kg.nodes
-            l = [str(x) for x in nodes]
-            return l
 
-        def get_links_query(self, query):
-            query_links = set()
-            results = bing_search(query, 2)
-            for each_link in results:
-                query_links.add(each_link['display_url'])
-            return query_links
-  
-        def dump_links_file(self):
-            with open('my_new_seeds.txt', 'w') as f:
-                for link in self.links:
-                    f.write("%s \n", link)
-            return
+	def get_existing(self, file_name):
+		l = []
+		with open(file_name, 'r') as f:
+			l = f.readlines()
+		res = [x.strip() for x in l]
+		return res
+
+	def dump_visited_queries(self, nodes):
+		with open('seeds_generator/visited_queries.txt', 'a') as f:
+			for each_node in nodes:
+				f.write(each_node)
+				f.write("\n")
+		return
+
+	def get_graph_nodes(self):
+		visited_queries = self.get_existing('seeds_generator/visited_queries.txt')
+		gqf = GraphQueryFormulator()
+		nodes = gqf.kg.nodes
+		l = []
+		for x in nodes:
+			if(str(x) not in visited_queries):
+				l.append(str(x))
+		return l
+
+	def get_links_query(self, query, f):
+		results = bing_search(query, 10)
+		for each_link in results:
+			try:
+				resp = requests.get(each_link['Url'])
+				f.write(resp.url.encode('utf-8'))
+				f.write("\n")
+			except Exception, e:
+				print("random error", e)
+				continue
+		return
 
 	def handle(self, *args, **options): 
-            queries = self.get_graph_nodes()[:2]
-            p = Pool(4)
-            func = partial(get_links_query)
-            results = p.map(func, queries)
-            for each in results:
-                self.links |= results
+		# get query nodes
+		queries = self.get_graph_nodes()
+		
+		f = open('seeds_generator/seeds.txt', 'as')
+		
+		for query in queries:
+			self.get_links_query(query, f)
+
+		f.close()
+
+		self.dump_visited_queries(queries)
+		return
                 
 
--- create_lesson_plan/pyms_cog.py
+++ create_lesson_plan/pyms_cog.py
@@ -2,6 +2,7 @@ from py_ms_cognitive import PyMsCognitiveWebSearch
 
 def bing_search(query, limit):
 	all_links = []
+	print('here!')
 	try:
 		API_KEY ='46b69e97a013424e8543dd082e977e74'
 		search_service = PyMsCognitiveWebSearch(API_KEY, 
@@ -10,9 +11,11 @@ def bing_search(query, limit):
 		response = search_service.search(
 			limit=limit, 
 			format='json')
+		print('here12!')
 
 		for link in response:
 			details = link.json
+			print(details)
 			link_details = {
 				'title': details['name'],
 				'Url': details['url'],
