1e3d79fc74b9a8739e2e374c143eeaf48486233f
rishabhranawat <rishabhranawat12345@gmail.com>
2017-11-10 01:06:51 -0500

    pdf extractor algortihms clrs learning from physical distance

--- create_lesson_plan/graph_query/graph_extractor/pdf_graph_extractor.py
+++ create_lesson_plan/graph_query/graph_extractor/pdf_graph_extractor.py
@@ -1,2 +1,16 @@
 import pdfquery
-pdf = pdf
+pdf =  pdfquery.PDFQuery("/Users/rishabh/Desktop/algorithms_clrs.pdf")
+pdf.load(6)
+
+tree = pdf.tree
+root = tree.getroot()
+
+allds = []
+for each in root.getchildren():
+	for p in each.getchildren():
+		for d in p.getchildren():
+			allds.append(d)
+
+allds.sort(key=lambda x: x.attrib['height'], reverse=True)
+for each in allds:
+	print(each.text, each.attrib['height'])
