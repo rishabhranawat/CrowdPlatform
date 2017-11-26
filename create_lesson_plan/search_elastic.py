5135bc0a66e670d6c4871ef7576aa8e8187b4103
rishabhanawat <rishabhranawat12345@gmail.com>
2017-11-26 17:04:17 -0500

    elasticsearch changesc

--- create_lesson_plan/search_elastic.py
+++ create_lesson_plan/search_elastic.py
@@ -7,52 +7,52 @@ client = Elasticsearch()
 
 class ElasticsearchOfflineDocuments():
 	def __init__(self):
-		self.gqf = GraphQueryFormulator()	
-
-	def get_query_input_title(self, input_title):
-		q_input_title = Q("match", content=input_title)
-		return q_input_title
+		self.gqf = GraphQueryFormulator()
 
 	def get_query_lesson_outline(self, lesson_outline):
 		q_lesson_outline = []
 		for bullet in lesson_outline:
+                    print(bullet)
                     q_lesson_outline.append(Q("match", content=bullet))
-		q_outlines = Q('bool', should=q_lesson_outline, minimum_should_match=0)
+		q_outlines = Q('bool', should=q_lesson_outline, minimum_should_match=1)
 		return q_outlines		
 
-	def get_query_link(self):
-		q_syllabus = ~Q("wildcard", link="*syllabus*")
-                q_wiki = ~Q("wildcard", link="*wikipedia*")
-                q_edu = Q("wildcard", link="*\.pdf*")
-                q_link = q_syllabus & q_wiki & q_edu
-	        
-                return q_link
-	
 	def get_required_links(self, hits):
 		links = []
 		for hit in hits:
 			if("syllabus" not in str(hit.link)): links.append(hit.link)
 		return set(links)
 
+        def get_diversity_links(self, phase):
+                if(phase == 1):
+                    q_wiki = Q("wildcard", link="*wikipedia*")
+                    q_edu = Q("wildcard", link="*\.edu*")
+                    q_edu_pdf = Q("wildcard", link="*.pdf*")
+                    q_random = ~q_wiki & ~q_edu
+
+                    query_types= [(q_wiki, 4), (q_edu, 5), (q_random, 5)]
+                    return query_types
+                else:
+                    print('here1')
+                    q_link_m = Q("wildcard", link="*midterm*")
+                    q_link_e = Q("wildcard", link="*exam*")
+                    q_link_f = Q("wildcard", link="*final*")
+                    q_link_s = Q("wildcard", link="*solutions*")
+                    q_link_hw = Q("wildcard", link="*hw*")
+                    q_link_h = Q("wildcard", link="*homework*")
+
+                    q_link = q_link_m or q_link_e or q_link_f or q_link_s \
+                            or q_link_hw or q_link_h
+                    return [(q_link, 20)]
+
 	def generate_search_urls(self, input_title, lesson_outline, phase=1, source=""):
 		s = Search(using=client, index="offline_content")
-
-        print(lesson_outline)
-		#input_title_q = self.get_query_input_title(input_title)
-		#link_q = self.get_query_link()
 		lesson_outline_q = self.get_query_lesson_outline(lesson_outline)
-
+                print(lesson_outline)
 		results = set()
-		q_wiki = Q("wildcard", link="*wikipedia*")
-		q_edu = Q("wildcard", link="*\.edu*")
-		q_must = Q("wildcard", content="*"+lesson_outline[0].replace(" ", "\ ")+"*")
-		q_edu_pdf = Q("wildcard", link="*.pdf*")
-		q_random = ~q_wiki & ~q_edu
-
-		query_types= [(q_wiki, 4), (q_edu, 5), (q_random, 5)]
-		for each_type in query_types:
+		query_types = self.get_diversity_links(phase)
+                for each_type in query_types:
 		    query = lesson_outline_q & each_type[0]
-		    #res = s.query(query)[:each_type[1]]
 		    res = s.query(query)[:each_type[1]]
 		    hits = res.execute()
 		    results |= self.get_required_links(hits)
