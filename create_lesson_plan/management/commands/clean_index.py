3f2e0a981093efb00e35887a4e387aec88d9f3ca
rishabhranawat <rishabhranawat12345@gmail.com>
2018-01-07 14:36:40 +0530

    clean index

---
+++ create_lesson_plan/management/commands/clean_index.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from elasticsearch import Elasticsearch
+from create_lesson_plan.models import IndexDocument
+
+class Command(BaseCommand):
+	def get_body(self, pk):
+		query_body = 		
+			{
+			  "query": {
+				"bool": {
+				  "must": [
+					{"match": {
+						"pk": pk
+					}}
+				  ]
+				}
+			  },
+			  "_source": ["link", "pk"]
+			}
+		return query_body
+
+	def handle(self, *args, **options):
+		all_index = IndexDocument.objects.all()
+		es = Elasticsearch()
+
+		for each in all_index.iterator():
+			query_body = self.get_body(each.pk)
+			results = es.search(index="offline_content", body=query_body)
+			print(results["hits"]["hits"][0]["_source"])
+			break
+		
