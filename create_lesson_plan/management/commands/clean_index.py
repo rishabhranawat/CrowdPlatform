6ad7dd6c2c4b27ea9b595dee431e40dfcb473a28
EC2 Default User <ec2-user@ip-172-31-22-239.us-west-2.compute.internal>
2018-01-08 04:09:09 +0000

    clean index changes

--- create_lesson_plan/management/commands/clean_index.py
+++ create_lesson_plan/management/commands/clean_index.py
@@ -5,8 +5,7 @@ from create_lesson_plan.models import IndexDocument
 
 class Command(BaseCommand):
 	def get_body(self, pk):
-		query_body = 		
-			{
+		query_body ={
 			  "query": {
 				"bool": {
 				  "must": [
@@ -23,10 +22,14 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		all_index = IndexDocument.objects.all()
 		es = Elasticsearch()
-
+                
+                counter = 0
 		for each in all_index.iterator():
 			query_body = self.get_body(each.pk)
 			results = es.search(index="offline_content", body=query_body)
-			print(results["hits"]["hits"][0]["_source"])
-			break
-		
+			if(len(results["hits"]["hits"]) > 0):
+                            res = results["hits"]["hits"][0]["_source"]
+                            if(res["link"] != each.link): 
+		                counter += 1
+                print(counter)
+
